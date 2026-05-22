713b2b10ca352bb8ce487ae078f23ef97b71df0b
KennyKvn001 <k.mugisha@alustudent.com>
2026-05-22 17:21:59 +0200

    updating the nav menu

--- my-portfolio/src/components/layout/TopNavBar.tsx
+++ my-portfolio/src/components/layout/TopNavBar.tsx
@@ -10,7 +10,6 @@ export function TopNavBar() {
       <div className="nav-links hidden md-flex">
         <a href="#stack" className="nav-link font-label-mono">STK/OVR</a>
         <a href="#projects" className="nav-link font-label-mono active">PRJ/LOG</a>
-        <a href="#experience" className="nav-link font-label-mono">EXP/DIR</a>
         <a href="#contact" className="nav-link font-label-mono">CNT/INF</a>
       </div>
 
